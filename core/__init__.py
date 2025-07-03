7205c1b5fca91f48947653be46428625e13773b7
codeguybutbackrooms <nhattruong792000@gmail.com>
2025-07-04 00:48:34 +0700

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,11 @@
 # core/__init__.py
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # Supported file extensions for media cutting
 SUPPORTED_FORMATS = ['.mp4', '.mp3', '.wav', '.webm', '.ogg']
 
 # Re-export core functions
-from .utils import generate_new_filename, check_disk_space, is_supported_file
+from commands.utils import generate_new_filename, check_disk_space, is_supported_file
 from .error import print_error
 from .ffmpeg import build_ffmpeg_command
